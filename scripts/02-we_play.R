9f27c131ac19528ee18ad05b4b6391f54693dbe7
Bernard Surial <surial.bernard@gmail.com>
2022-02-02 10:56:34 +0100

    add summary write data

--- scripts/02-we_play.R
+++ scripts/02-we_play.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(bernr)
 library(here)
+library(gtsummary)
 
 
 
@@ -18,7 +19,7 @@ pro_read <- function(file) {
 # Read data ---------------------------------------------------------------
 pat <- pro_read("pat.rds")
 lab <- pro_read("lab.rds")
-
+tail <- pro_read("tail.rds")
 
 
 
@@ -31,6 +32,38 @@ hcv_pos <- lab %>%
   group_by(id) %>%
   summarise(any_hcv_rna = any(hcv_rna >= 50, na.rm = TRUE)) %>% 
   filter(any_hcv_rna == TRUE)
+
+
+
+
+
+# Add demographics --------------------------------------------------------
+demographics <- tail %>% 
+  select(id, sex, born, riskgroup)
+
+
+full <- hcv_pos %>% 
+  left_join(demographics)
+
+
+
+full %>% 
+  select(-id) %>% 
+  mutate(age = 2022 - born) %>% 
+  select(-born) %>% 
+  tbl_summary(by = sex, missing = "always") %>% 
+  add_overall()
+
+
+
+  
+
+# Write data --------------------------------------------------------------
+
+write_rds(full, here("processed", "02-study_population.rds"))
+
+
+
 
 
   
